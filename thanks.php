aa485e4c5b4ccca4001721a92aa7aa3a74ed1b1d
unknown <florem.10@hotmail.com>
2015-09-08 10:37:55 -0300

    pagina respuesta

--- thanks.php
+++ thanks.php
@@ -15,7 +15,96 @@ include("/include_extra/head.php");
 	?>
 
 	<section class="thanks">
-		<h1>¡Muchas gracias por comprar<br>y confiar en nosotros!</h1>
+		<h1>Tu pago se ha acreditado con éxito.</h1>
+		<h1 style="display:none">El pago ha sido rechazado por fondos insuficientes.</h1>
+		<h1 style="display:none">El pago ha sido rechazado por exceso del límite.</h1>
+		<h1 style="display:none">La sesión ha expirado. Inténtelo nuevamente.</h1>
+		<h1 style="display:none">Ha ocurrido un inconveniente. Inténtelo de nuevo por favor.</h1>
+		
+		<div class='info'>
+			<table class='head'>
+				<tr>
+					<td class='head cod'>N° de pago</td>
+				</tr>
+				<tr>
+					<td class='head'>Titular</td>
+				</tr>
+				<tr>
+					<td class='head'>Forma de pago</td>
+				</tr>
+				<tr>
+					<td class='head'>Monto total</td>
+				</tr>
+				<tr>
+					<td class='head'>Fecha</td>
+				</tr>
+			</table>
+			
+			<table class='info'>
+				<tr>
+					<td class='cod'>153682-2</td>
+				</tr>
+				<tr>
+					<td>Acosta, M. Florencia</td>
+				</tr>
+				<tr>
+					<td>Visa, 6 cuotas</td>
+				</tr>
+				<tr>
+					<td>$420</td>
+				</tr>
+				<tr>
+					<td>07/09/2015</td>
+				</tr>
+			</table>
+		</div>
+		<div class='clear'></div>
+		
+			<h2>¡Importante!</h2>
+			<p>En caso de requerir el envío de la compra llenar el siguiente formulario y a la brevedad enviaremos el costo del despacho.</p>
+				<form class='formulario-envio' enctype="text/plain" method="get" action="mailto:ventas@flopysystem.com.ar">
+					<div>
+						<ul>
+							<li>
+								<h2>Dirección</h2>
+								<input type="text" value="" name='direccion' maxlength="50"></input>
+							</li>
+							<li>
+								<h2>Número</h2>
+								<input type="text" value="" name='altura' maxlength="6"></input>
+							</li>
+							<li>
+								<h2>Piso*</h2>
+								<input type="text" value="" name='piso' maxlength="2"></input>
+							</li>
+							<li>
+								<h2>Departamento*</h2>
+								<input type="text" value="" name='departamento' maxlength="2"></input>
+							</li>
+						</ul>	
+						<ul>
+							<li>
+								<h2>Localidad</h2>
+								<input type="text" value="" name='localidad' maxlength="40"></input>
+							</li>
+							<li>
+								<h2>Provincia</h2>
+								<input type="text" value="" name='provincia' maxlength="40"></input>
+							</li>
+							<li>
+								<h2>Código Postal</h2>
+								<input type="text" value="" name='codigoPostal' maxlength="4"></input>
+							</li>
+							<li>
+								<h2>E-Mail</h2>
+								<input type="text" value="" name='mail' maxlength="25"></input>
+							</li>
+						</ul>
+					</div>				
+					<div class='clear'></div>
+					<input type="submit" value="Enviar" ></input>
+				</form>
+			</div>
 		<a href="/index.html"><h2>Página principal</h2></a>
 	</section>
 
